f902af8fd98c85ec95469916332b5c6bbe37d94b
taishuang4687 <18845772690@163.com>
2019-06-18 20:19:26 +0800

    node 练习

--- upload/uploads/server.js
+++ upload/uploads/server.js
@@ -1,23 +1,37 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var util = require('util');
+var http=require('http');
+var url=require('url');
+var fs=require('fs');
+var path=require('path');
+var util=require('util');
+var formidable=require('formidable');
 http.createServer(function(req,res){
     var pathname = url.parse(req.url).pathname;
     switch(pathname){
         case '/':
             goIndex(res);
+            break;
         case '/parse':
             goUpload(req,res);
+            break;
         default:
-            res.writeHead(404,{'content-type':'text/plain'});
+            res.writeHead(404,{'Content-type':'text/plain'});
             res.end('the page is gone');
     }
 }).listen(3000);
-function goIndex(res){
-    var pathname = __dirname + '/static/' + url.parse("index.html").pathname;
-    var readfile = fs.readFileSync(pathname,'utf-8');
-    res.writeHead(200,{'content-type':'text/html'});
-    res.end(readfile);
-}
 console.log('server start port 3000');
+function goIndex(res){
+    var pathname=path.parse(__dirname).dir+'/static/'+url.parse('index.html').pathname;
+    var readfile=fs.readFileSync(pathname,'utf-8');
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.end(readfile);
+};
+function goUpload(req,res){
+    var form = new formidable.IncomingForm();
+    form.uploadDir = '/my/dir';
+    form.parse(req,function(err,fields,files){
+        res.writeHead(200,{'content-type':'text/plain'});
+        res.write('received upload:\n\n');
+        res.end(util.inspect({fields:fields,files:files}));
+    });
+    return;
+}
